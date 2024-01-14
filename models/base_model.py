7dfbf8d96144b1b2a1704979e9005cb338398f3f
Mulih <almulih@yahoo.com>
2024-01-15 00:40:10 +0300

    Upload of engine

--- models/base_model.py
+++ models/base_model.py
@@ -43,7 +43,7 @@ class BaseModel:
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
-            self.updated_at = selfeated_at
+            self.updated_at = self.created_at
     
     def __str__(self):
         """Returns the string representation of the instance of the class
