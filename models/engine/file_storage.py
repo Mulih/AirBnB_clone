caa613a331f8f74f4de127ab2e7f467ce60847b2
Mulih <almulih@yahoo.com>
2024-01-15 04:57:19 +0300

    Import error fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 import json
 from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
